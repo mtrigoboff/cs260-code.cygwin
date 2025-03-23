ac52bb4a9d07ddccb34172dc61e520a9cd955938
michael-trigoboff <mtrigoboff@gmail.com>
2025-03-23 15:44:22 -0700

    prints out built-in datatype sizes

---
+++ data_sizes/data_sizes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "char:      " << sizeof(char) << endl;
+	cout << "short:     " << sizeof(short) << endl;
+	cout << "int:       " << sizeof(int) << endl;
+	cout << "long:      " << sizeof(long) << endl;
+	cout << "long long: " << sizeof(long long) << endl;
+	cout << "float:     " << sizeof(float) << endl;
+	cout << "double:    " << sizeof(double) << endl;
+}
